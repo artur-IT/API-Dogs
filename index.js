f7b69243289001be065bc9da3bf37856792d59b7
Artur Matysiak <127504662+artur-IT@users.noreply.github.com>
2023-05-10 13:33:15 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -44,7 +44,6 @@ class DogsBreeds {
 
   showBreeds = (list) => {
     const breedsE = Object.entries(list);
-    const divLink = document.body.querySelector("div.breeds__link");
 
     // SUBBREED
     breedsE.forEach((elem) => {
@@ -54,41 +53,80 @@ class DogsBreeds {
             .then((img) => {
               return img.json();
             })
-
             .then((img) => {
-              tabBreeds.push({
-                name: `${elem[0]} ${elem[1][i]}`,
-                link: img.message,
-              });
+              tabBreeds.push(`${elem[0]} ${elem[1][i]}`, img.message);
             });
         }
-      } else {
-        fetch(`${urlMain}/breed/${elem[0]}/images/random`)
-          .then((img) => {
-            return img.json();
-          })
-
-          .then((img) => {
-            tabBreeds.push({
-              name: `${elem[0]}`,
-              link: img.message,
-            });
-          });
       }
+
+      fetch(`${urlMain}/breed/${elem[0]}/images/random`)
+        .then((img) => {
+          return img.json();
+        })
+        .then((img) => {
+          tabBreeds.push(elem[0], img.message);
+        });
     });
+
+    // GOOD OLD CODE - build tab of breeds and subbreeds
+    // breedsE.forEach((elem) => {
+    //   if (elem[1] != 0) {
+    //     for (let i = 0; i < elem[1].length; i++) {
+    //       fetch(`${urlMain}/breed/${elem[0]}/${elem[1][i]}/images/random`)
+    //         .then((img) => {
+    //           return img.json();
+    //         })
+
+    //         .then((img) => {
+    //           tabBreeds.push({
+    //             name: `${elem[0]} ${elem[1][i]}`,
+    //             link: img.message,
+    //           });
+    //         });
+    //     }
+    //   } else {
+    //     fetch(`${urlMain}/breed/${elem[0]}/images/random`)
+    //       .then((img) => {
+    //         return img.json();
+    //       })
+
+    //       .then((img) => {
+    //         tabBreeds.push({
+    //           name: `${elem[0]}`,
+    //           link: img.message,
+    //         });
+    //       });
+    //   }
+    // });
   };
 }
 
+// add to DOM tiles
 const toHTML = () => {
   const divLink = document.body.querySelector("div.breeds__link");
-  for (let i = 0; i < tabBreeds.length; i++) {
+  for (let i = 0; i < tabBreeds.length; ) {
+    let x = 1;
     const a = document.createElement("a");
     divLink.appendChild(a);
     a.classList.add("breeds__link__dog");
-    a.textContent = `${tabBreeds[i].name} `;
-    a.setAttribute("id", `${tabBreeds[i].link}`);
+    a.textContent = `${tabBreeds[i]} `;
+    a.setAttribute("id", `${tabBreeds[i + x]}`);
+    i += 2;
   }
 };
+
+// GOOD OLD CODE - add to DOM tiles
+// const toHTML = () => {
+//   console.log(tabBreeds);
+//   const divLink = document.body.querySelector("div.breeds__link");
+//   for (let i = 0; i < tabBreeds.length; i++) {
+//     const a = document.createElement("a");
+//     divLink.appendChild(a);
+//     a.classList.add("breeds__link__dog");
+//     a.textContent = `${tabBreeds[i].name} `;
+//     a.setAttribute("id", `${tabBreeds[i].link}`);
+//   }
+// };
 
 new DogRandom();
 new DogsBreeds();
@@ -96,6 +134,7 @@ new DogsBreeds();
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
     toHTML();
+    // add to DOM img with description
     const test = document.getElementsByClassName("breeds__link__dog");
     const div = document.querySelector("div.breed__dog");
     const divRandom = document.querySelector("div.random h1");
@@ -111,5 +150,3 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }, 1500);
 });
-
-// test;
